e7273c4ad61c2b9db7f62e2a8c714a691af60e5a
Mathias Gredal <Mathiasgredal@icloud.com>
2020-02-26 08:57:08 +0100

    Setup CMake

--- main.cpp
+++ main.cpp
@@ -1,21 +1,18 @@
-#include <vector>
-#include <string>
-#include <iostream>
 #include "data_layer.h"
 #include "presentation_layer.h"
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 int main()
 {
     vector<string> games;
 
-    try
-    {
+    try {
         games = data::get_games_from_file();
         presentation::display_best(games[0]);
-    }
-    catch(exception& e)
-    {
+    } catch (exception& e) {
         cerr << e.what() << endl;
     }
 
--- src/data_layer.cpp
+++ src/data_layer.cpp
@@ -1,9 +1,8 @@
 #include "data_layer.h"
-#include <fstream>  // filestream
 #include <algorithm>
+#include <fstream> // filestream
 
-namespace data
-{
+namespace data {
 
 std::vector<std::string> get_games_from_file()
 {
@@ -16,32 +15,29 @@ std::vector<std::string> get_games_from_file()
 
     // Open file
     string filename = "data/top100bgg.csv";
-    ifstream file{filename};
-    if(!file)
-    {
+    ifstream file(filename);
+
+    if (!file) {
         throw runtime_error("Error - Couldn't open file: " + filename);
     }
 
     // Skip first line (titles). Then read every line after.
     getline(file, line);
-    while(getline(file, line))
-    {
+    while (getline(file, line)) {
         // Initialize current index in string and find first delimiter ";"
         current = 0;
         next = line.find(';');
 
-        for(int i = 0; next > 0; i++ )
-        {
+        for (int i = 0; next > 0; i++) {
             // Get the next word and update indices to next word
-            word = line.substr(current,next-current);
-            current = next+1;
-            next = line.find(';',current);
+            word = line.substr(current, next - current);
+            current = next + 1;
+            next = line.find(';', current);
 
             // Push current word to corresponding vector
-            switch(i)
-            {
+            switch (i) {
             case 0:
-                ranking.push_back(stoi(word));  // stoi is String TO Int
+                ranking.push_back(stoi(word)); // stoi is String TO Int
                 break;
             case 1:
                 games.push_back(word);
